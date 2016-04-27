d4cec5ad844b9d55bdb9bfbde26398cabf2f8038
xtofux <felks.exe@gmail.com>
2016-04-27 15:03:50 -0300

    Added files via upload

---
+++ settings.js
@@ -0,0 +1,9 @@
+var settings = {
+	"USERNAME": "TofuBot",
+	"PASSWORD": "senha123",
+	"ROOMNAME": "rock-e-mpb	",
+	"MONGODBURL": "mongodb://localhost:27017/TofuBot",
+	"SOUNDCLOUDID": "soundcloudApiId"
+};
+
+module.exports.settings = settings;
